8efa3fe78e731a3ca7691c24e3cdcd5909df4348
ShonBC <cortesshon@gmail.com>
2022-02-12 18:23:12 -0800

    Add documentation for all class methods

--- Homework_2/cmac.py
+++ Homework_2/cmac.py
@@ -18,6 +18,17 @@ class DiscreteCMAC(CMAC):
         CMAC.__init__(self, generalization_factor, num_weights)
 
     def AssocVecIdx(self, input_value, num_association_vectors, min_input, max_input):
+        """Determine the association vector index to be assigned in the association map for a given input data point.
+
+        Args:
+            input_value (float]): Input data value
+            num_association_vectors (int): Number of association vectors to generate
+            min_input (float): Min input value
+            max_input (float): Max input value
+
+        Returns:
+            (float): Index to be assigned in the association map
+        """
         if input_value < min_input:
             return 1
         elif input_value > max_input:
@@ -27,17 +38,37 @@ class DiscreteCMAC(CMAC):
             return proportion_idx
 
     def GenerateAssociationMap(self, data, min_input, max_input):
+        """Generate the association map dictionary of indexes (value) for each input data point (key).
+
+        Args:
+            data (list): List of input data points
+            min_input (float): Min input value
+            max_input (float): Max input value
+        """
+
+        # Calculate number of association vectors to generate
         num_association_vectors = self.num_weights + 1 - self.generalization_factor
 
-        for idx, value in data:
-            # enumerate()
-            association_vec_idx = self.GetAssociationVecIdx(value[0],
-                                                            num_association_vectors, 
-                                                            min_input, 
-                                                            max_input)
+        # For each data point (key), determine its association vector index and 
+        # assign it to the association map (value)
+        for value in data:
+            association_vec_idx = self.AssocVecIdx(value[0],
+                                                    num_association_vectors, 
+                                                    min_input, 
+                                                    max_input)
             self.association_map[value[0]] = int(math.floor(association_vec_idx)) # Round index down
 
     def CalcError(self, predicted, expected):
+        """Calculate the error of the model based of the difference in expected and predicted values.
+
+        Args:
+            predicted (list): List of predicted outputs from the CMAC model
+            expected (list): List of expected outputs predetermined from input data labels
+
+        Returns:
+            err_total (float): The percentage of predictions the model was able to correctly determine in decimal.
+        """
+
         err = np.subtract(expected, predicted)
         sum_err_squared = np.sum(np.power(err, 2))
         err_total = np.sqrt(sum_err_squared) / len(expected)
@@ -45,28 +76,54 @@ class DiscreteCMAC(CMAC):
         return err_total
 
     def Predict(self, data, min_input, max_input, gen_assoc_map = True):
-        output = []
+        """Generate predictions using the CMAC model using input data points
+
+        Args:
+            data (list): List of input data points
+            min_input (float): Min input value
+            max_input (float): Max input value
+            gen_assoc_map (bool, optional): Determines if the association map will get generated again. Defaults to True.
+
+        Returns:
+            predicted (list): List of predicted outputs
+            accuracy (float): The percentage of predictions the model was able to correctly determine in decimal.
+        """
+
+        predicted = []
 
         if gen_assoc_map:
             self.GenerateAssociationMap(data, min_input, max_input)
 
-        for idx, value in data:
-            #enumerate(data)
+        for value in data:
             weight_idx = self.association_map[value[0]]
 
             # Sum the weights in activated cells
             temp_output = np.sum(self.weight_vector[weight_idx : weight_idx + self.generalization_factor])
 
-            output.append(temp_output)
+            predicted.append(temp_output)
 
-        err = self.CalcError(output, data[ : , 1])
+        expected = data[ : , 1]
+        err = self.CalcError(predicted, expected)
         accuracy = 1 - err
 
-        return output, accuracy
+        return predicted, accuracy
 
     def TrainModel(self, data, min_input, max_input, epochs = 10000, learning_rate = 0.01):
+        """Train the Discrete CMAC model
+
+        Args:
+            data (list): List of input data points
+            min_input (float): Min input value
+            max_input (float): Max input value
+            epochs (int, optional): Max number of times the model will iterate in an attempt to converge. Defaults to 10000.
+            learning_rate (float, optional): Step value resolution used for correction of weights. Defaults to 0.01.
+
+        Returns:
+            time_delta (float): How long the model took to train
+        """
+        
+        # Initialize model parameters
         self.weight_vector = np.ones(self.num_weights)
-        # inputs = np.linspace(min_input, max_input)
         current_epoch = 0
         self.association_map = {}
         self.GenerateAssociationMap(data, min_input, max_input)
@@ -77,17 +134,26 @@ class DiscreteCMAC(CMAC):
         start_time = time.clock()
         while current_epoch <= epochs and not converged:
             prev_err = current_err
-            for idx, value in data:
-                #enumerate(data)
+
+            for value in data:
+                # Get index for the beginning of generalization factor window
                 weight_idx = self.association_map[value[0]]
+
+                # Output is the sum of the weights within the generalization factor window
                 output = np.sum(self.weight_vector[weight_idx : weight_idx + self.generalization_factor])
                 err = value[1] - output
                 correction = (learning_rate * err) / self.generalization_factor
-                self.weight_vector[weight_idx : weight_idx + self.generalization_factor] = [(self.weight_vector[idx] + correction) for idx in range(weight_idx, (weight_idx + self.generalization_factor))]
-            
+                
+                # Recalculate the weight vector values using the correction coeff
+                self.weight_vector[weight_idx : weight_idx + self.generalization_factor] = \
+                    [(self.weight_vector[idx] + correction) \
+                    for idx in range(weight_idx, (weight_idx + self.generalization_factor))]
+
+           # Calculate the current accuracy of the model 
             _, accuracy = self.Predict(data, min_input, max_input, False)
             current_err = 1 - accuracy
 
+            # Check if the model has congerged
             if np.abs(prev_err, current_err) < 0.0000001:
                 converged = True
                         
@@ -103,6 +169,18 @@ class ContinuousCMAC(CMAC):
         CMAC.__init__(generalization_factor, num_weights)
     
     def AssocVecIdx(self, input_value, num_association_vectors, min_input, max_input):
+        """Determine the association vector index to be assigned in the association map for a given input data point.
+
+        Args:
+            input_value (float]): Input data value
+            num_association_vectors (int): Number of association vectors to generate
+            min_input (float): Min input value
+            max_input (float): Max input value
+
+        Returns:
+            (float): Index to be assigned in the association map
+        """
+
         if input_value < min_input:
             return 1
         elif input_value > max_input:
@@ -112,14 +190,24 @@ class ContinuousCMAC(CMAC):
             return proportion_idx
 
     def GenerateAssociationMap(self, data, min_input, max_input):
+        """Generate the association map dictionary of indexes (value) for each input data point (key).
+
+        Args:
+            data (list): List of input data points
+            min_input (float): Min input value
+            max_input (float): Max input value
+        """
+        
+        # Calculate number of association vectors to generate
         num_association_vectors = self.num_weights + 1 - self.generalization_factor
 
-        for idx, value in data:
-            # enumerate(data)
-            association_vec_idx = self.GetAssociationVecIdx(value[0],
-                                                            num_association_vectors, 
-                                                            min_input, 
-                                                            max_input)
+        # For each data point (key), determine its association vector index and 
+        # assign it to the association map (value)
+        for value in data:
+            association_vec_idx = self.AssocVecIdx(value[0],
+                                                    num_association_vectors, 
+                                                    min_input, 
+                                                    max_input)
             # Round index up and down
             low_association_vec_idx = int(math.floor(association_vec_idx))
             high_association_vec_idx = int(math.ceil(association_vec_idx))
@@ -130,6 +218,16 @@ class ContinuousCMAC(CMAC):
                 self.association_map[value[0]] = (low_association_vec_idx, 0)
     
     def CalcError(self, predicted, expected):
+        """Calculate the error of the model based of the difference in expected and predicted values.
+
+        Args:
+            predicted (list): List of predicted outputs from the CMAC model
+            expected (list): List of expected outputs predetermined from input data labels
+
+        Returns:
+            err_total (float): The percentage of predictions the model was able to correctly determine in decimal.
+        """
+
         err = np.subtract(expected, predicted)
         sum_err_squared = np.sum(np.power(err, 2))
         err_total = np.sqrt(sum_err_squared) / len(expected)
@@ -137,14 +235,26 @@ class ContinuousCMAC(CMAC):
         return err_total
 
     def Predict(self, data, min_input, max_input, gen_assoc_map = True):
-        output = []
+        """Generate predictions using the Continuous CMAC model using input data points
+
+        Args:
+            data (list): List of input data points
+            min_input (float): Min input value
+            max_input (float): Max input value
+            gen_assoc_map (bool, optional): Determines if the association map will get generated again. Defaults to True.
+
+        Returns:
+            predicted (list): List of predicted outputs
+            accuracy (float): The percentage of predictions the model was able to correctly determine in decimal.
+        """
+
+        predicted = []
         inputs = np.linspace(min_input, max_input, self.num_weights + 1 - self.generalization_factor)
 
         if gen_assoc_map:
             self.GenerateAssociationMap(data, min_input, max_input)
 
-        for idx, value in data:
-            #enumerate(data)
+        for value in data:
             low_association_idx = self.association_map[value[0][0]]
             high_association_idx = self.association_map[value[0][1]]
 
@@ -157,14 +267,27 @@ class ContinuousCMAC(CMAC):
             # Sum the weights in activated cells
             temp_output = (l_ratio * np.sum(self.weight_vector[low_association_idx : low_association_idx + self.generalization_factor])) + (r_ratio * np.sum(self.weight_vector[high_association_idx : high_association_idx + self.generalization_factor]))
 
-            output.append(temp_output)
+            predicted.append(temp_output)
 
-        err = self.CalcError(output, data[ : , 1])
+        err = self.CalcError(predicted, data[ : , 1])
         accuracy = 1 - err
 
-        return output, accuracy
+        return predicted, accuracy
 
     def Train(self, data, min_input, max_input, epochs = 10000, learning_rate = 0.01):
+        """Train the Continuous CMAC model
+
+        Args:
+            data (list): List of input data points
+            min_input (float): Min input value
+            max_input (float): Max input value
+            epochs (int, optional): Max number of times the model will iterate in an attempt to converge. Defaults to 10000.
+            learning_rate (float, optional): Step value resolution used for correction of weights. Defaults to 0.01.
+
+        Returns:
+            time_delta (float): How long the model took to train
+        """
+
         self.weight_vector = np.ones(self.num_weights)
         current_epoch = 0
         self.association_map = {}
@@ -177,8 +300,8 @@ class ContinuousCMAC(CMAC):
         start_time = time.clock()
         while current_epoch <= epochs and not converged:
             prev_err = current_err
-            for idx, value in data:
-                # enumerate(data)
+
+            for value in data:
                 low_association_idx = self.association_map[value[0][0]]
                 high_association_idx = self.association_map[value[0][1]]
 
@@ -203,7 +326,7 @@ class ContinuousCMAC(CMAC):
                 current_epoch = current_epoch + 1
                 print(f'Continuous CMAC: \n generalization factor: {self.generalization_factor} \n epoch: {current_epoch} \n error: {current_err} \n accuracy: {accuracy * 100}%')
 
-            end_time = time.clock()
-            time_delta = end_time - start_time
-            return time_delta
+        end_time = time.clock()
+        time_delta = end_time - start_time
+        return time_delta
 
