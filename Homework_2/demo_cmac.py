b44e9ea379efb491523d3ea45cfcbf4028eda428
ShonBC <cortesshon@gmail.com>
2022-02-13 23:23:01 -0800

    Document code and add tesing of model

--- Homework_2/demo_cmac.py
+++ Homework_2/demo_cmac.py
@@ -3,17 +3,68 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import matplotlib
 
-def Hw2Q2(train_x, train_y, min_input, max_input, generalization_factor, num_weights):
+def Hw2Q2(train_x, train_y, test_x, test_y, min_input, max_input, generalization_factor, num_weights):
+    """Program a Discrete CMAC and train it on a 1-D function (ref: Albus 1975, Fig. 5) 
+    Explore effect of overlap area on generalization and time to convergence. 
+    Use only 35 weights for your CMAC, and sample your function at 100 evenly spaced points. 
+    Use 70 for training and 30 for testing. Report the accuracy of your CMAC network using 
+    only the 30 test points.
+
+    Args:
+        train_x (list): Training input data
+        train_y (list): Training output data
+        test_x (list): Testing input data
+        test_y (list): Testing output data
+        min_input (float): Lowest input value
+        max_input (float): Largest input value
+        generalization_factor (int): Generalization factor
+        num_weights (int): Number of weights to generate
+
+    Returns:
+        class: Trained discrete CMAC model
+        list: Predicted outputs using the test data inputs
+        float: The percentage of predictions the model was able to correctly determine in decimal
+    """
+
     d_cmac = cmac.DiscreteCMAC(generalization_factor, num_weights)
     data = [train_x, train_y]
     d_cmac.TrainModel(data, min_input, max_input)
-    return d_cmac
+    
+    data = [test_x, test_y]
+    predicted, accuracy = d_cmac.Predict(data, min_input, max_input)
+    return d_cmac, predicted, accuracy
 
-def Hw2Q3(train_x, train_y, min_input, max_input, generalization_factor, num_weights):
+def Hw2Q3(train_x, train_y, test_x, test_y, min_input, max_input, generalization_factor, num_weights):
+    """ Program a Continuous CMAC by allowing partial cell overlap, and modifying the 
+    weight update rule accordingly. Use only 35 weights for your CMAC, and sample your 
+    function at 100 evenly spaced points.  Use 70 for training and 30 for testing. 
+    Report the accuracy of your CMAC network using only the 30 test points. Compare the 
+    output of the Discrete CMAC with that of the Continuous CMAC. 
+    (You may need to provide a graph to compare)
+
+    Args:
+        train_x (list): Training input data
+        train_y (list): Training output data
+        test_x (list): Testing input data
+        test_y (list): Testing output data
+        min_input (float): Lowest input value
+        max_input (float): Largest input value
+        generalization_factor (int): Generalization factor
+        num_weights (int): Number of weights to generate
+
+    Returns:
+        class: Trained continuous CMAC model
+        list: Predicted outputs using the test data inputs
+        float: The percentage of predictions the model was able to correctly determine in decimal
+    """
+
     c_cmac = cmac.ContinuousCMAC(generalization_factor, num_weights)
     data = [train_x, train_y]
     c_cmac.TrainModel(data, min_input, max_input)
-    return c_cmac
+
+    data = [test_x, test_y]
+    predicted, accuracy = c_cmac.Predict(data, min_input, max_input)
+    return c_cmac, predicted, accuracy
 
 if __name__ == '__main__':
     
@@ -29,14 +80,10 @@ if __name__ == '__main__':
     generalization_factor = 10 
     num_weights = 35
 
-    # Train Discrete CMAC
-    d_cmac = Hw2Q2(train_x, train_y, min_input, max_input, generalization_factor, num_weights)
-
-    # Test Discrete CMAC
+    # Train and Test Discrete CMAC
+    d_cmac, predicted, accuracy = Hw2Q2(train_x, train_y, test_x, test_y, min_input, max_input, generalization_factor, num_weights)
 
 
-    # Train Continuous CMAC
-    c_cmac = Hw2Q3(train_x, train_y, min_input, max_input, generalization_factor, num_weights)
 
-    # Test Continuous CMAC
-
+    # Train and Test Continuous CMAC
+    c_cmac, predicted, accuracy = Hw2Q3(train_x, train_y, test_x, test_y, min_input, max_input, generalization_factor, num_weights)
